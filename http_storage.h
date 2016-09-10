846437a04caf684e3dd97a9197e79f164de79891
Joel <sauskethunderkid@gmail.com>
2016-09-09 22:32:47 -0500

    Update complete reverse.
    This can be built using uOFW.

--- http_storage.h
+++
@@ -1,8 +0,0 @@
-s32 sceHttpStorageOpen();
-s32 sceHttpStorageClose();
-s32 sceHttpStorageRead();
-s32 sceHttpStorageWrite();
-s32 sceHttpStorageLseek();
-s32 sceHttpStorageGetstat();
-s32 sceHttpStorage_bridge_04EF00F8();
-s32 sceHttpStorage_bridge_EFE06A20();
